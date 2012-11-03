c81a000f6001f991336928bae3950355ca6bc0f7
Taiwo <taiwo@taiwo-1215P.(none)>
2012-11-03 19:51:12 -0400

    Preparing to deploy to Heroku

--- config/application.rb
+++ config/application.rb
@@ -57,8 +57,6 @@ module Shelflet
 
     config.assets.initialize_on_precompile = false
 
-
-
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
