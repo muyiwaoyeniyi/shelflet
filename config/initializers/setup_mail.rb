79e94d0cc9d453c5858e4dca5018dbec6573ef4c
Taiwo <taiwo@taiwo-1215P.(none)>
2012-11-04 00:15:05 -0400

    Preparing to deploy to Heroku

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :domain               => "shelflet.herokuapp.com",
+  :domain               => "shelflet.com",
   :user_name            => "team@shelflet.com",
   :password             => "Umbrella12",
   :authentication       => "plain",
