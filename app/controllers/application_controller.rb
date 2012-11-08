d95982841277a5aa7b8ea0e1cc2e0c5cb4212528
Taiwo <taiwo@taiwo-1215P.(none)>
2012-11-08 13:33:45 -0500

    Preparing to deploy to Heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,4 +33,16 @@ class ApplicationController < ActionController::Base
     # render file: "#{Rails.root}/public/403", formats: [:html], status: 403, layout: false
   end
 
+  require 'socket'
+
+  def local_ip
+    orig, Socket.do_not_reverse_lookup = Socket.do_not_reverse_lookup, true  # turn off reverse DNS resolution temporarily
+
+    UDPSocket.open do |s|
+      s.connect '64.233.187.99', 1
+      s.addr.last
+    end
+  ensure
+    Socket.do_not_reverse_lookup = orig
+  end
 end
