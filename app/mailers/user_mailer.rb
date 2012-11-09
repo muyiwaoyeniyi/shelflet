916d7418492db5bbbb9317951a2d56c54399ad51
Taiwo <taiwo@taiwo-1215P.(none)>
2012-11-09 13:51:24 -0500

    Preparing to deploy to Heroku

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,7 @@ class UserMailer < ActionMailer::Base
   def welcome_message(user)
     @user = user
     #attachments.inline["shelflet.jpg"] = File.read("#{Rails.root}/public/assets/shelflet.jpg")  
-    mail(:to => user.email, :subject => "Shelflet: Welcome to our Alpha launch")
+    mail(:to => user.email, :subject => "Shelflet | Welcome to our Alpha launch")
   end
 
   def contact_us_message(message)
